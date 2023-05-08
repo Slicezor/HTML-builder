b66fce5b1680f9be3b1321725195a6af843f0a2a
Stanislav Shershniov <stas.she@gmail.com>
2023-05-08 16:15:28 +0400

    feat: the fourth task "04-copy-directory" is done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fs = require('fs')
+const path = require('path')
 
+const sourceFolder = `${path.join('04-copy-directory', 'files')}`
+const copyFolder = `${path.join('04-copy-directory', 'files-copy')}`
+
+copyDir(sourceFolder, copyFolder)
+
+function copyDir(sourceFolder, copyFolder) {
+  fs.mkdir(copyFolder, { recursive: true }, (err) => {
+    if (err) {
+      console.error("Failed to create folder", err);
+      return
+    }
+
+    fs.readdir(sourceFolder, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        console.error("Failed to read the folder", err);
+        return
+      }
+
+      files.forEach((file) => {
+        const source = path.join(sourceFolder, file.name)
+        const copy = path.join(copyFolder, file.name)
+
+        if (file.isDirectory()) {
+          copyDir(source, copy)
+        } else {
+          fs.copyFile(source, copy, (err) => {
+            if (err) {
+              console.error("Failed to copy file", err);
+              return
+            }
+
+            console.log(`Copied file "${source}" to "${copy}"`)
+          })
+        }
+      })
+    })
+  })
+}
