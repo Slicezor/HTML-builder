43cd4592f416f0d0897eaeec1bb945868ea569a9
Stanislav Shershniov <stas.she@gmail.com>
2023-05-11 17:00:44 +0400

    fix: fix mistakes in task "04-copy-directory"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,41 +1,88 @@
 const fs = require('fs')
 const path = require('path')
 
-const sourceFolder = `${path.join('04-copy-directory', 'files')}`
-const copyFolder = `${path.join('04-copy-directory', 'files-copy')}`
+const sourceFolder = path.join('04-copy-directory', 'files')
+const copyFolder = path.join('04-copy-directory', 'files-copy')
 
-copyDir(sourceFolder, copyFolder)
+deleteFiles(copyFolder, () => {
+  createCopyFolder(copyFolder, () => {
+    copyDir(sourceFolder, copyFolder)
+  })
+})
 
-function copyDir(sourceFolder, copyFolder) {
-  fs.mkdir(copyFolder, { recursive: true }, (err) => {
+function deleteFiles(folder, callback) {
+  fs.readdir(folder, (err, files) => {
     if (err) {
-      console.error("Failed to create folder", err);
+      if (err.code === 'ENOENT') {
+        callback()
+      } else {
+        console.error("Failed to read the folder", err)
+      }
       return
     }
 
-    fs.readdir(sourceFolder, { withFileTypes: true }, (err, files) => {
-      if (err) {
-        console.error("Failed to read the folder", err);
-        return
-      }
+    let deletedCount = 0
 
-      files.forEach((file) => {
-        const source = path.join(sourceFolder, file.name)
-        const copy = path.join(copyFolder, file.name)
+    if (files.length === 0) {
+      callback()
+      return
+    }
 
-        if (file.isDirectory()) {
-          copyDir(source, copy)
-        } else {
-          fs.copyFile(source, copy, (err) => {
-            if (err) {
-              console.error("Failed to copy file", err);
-              return
-            }
+    files.forEach((file) => {
+      const filePath = path.join(folder, file)
 
-            console.log(`Copied file "${source}" to "${copy}"`)
-          })
+      fs.unlink(filePath, (err) => {
+        deletedCount++
+
+        if (err) {
+          console.error("Failed to delete file", err)
+          return
+        }
+
+        if (deletedCount === files.length) {
+          callback()
         }
       })
     })
+  })
+}
+
+function createCopyFolder(folder, callback) {
+  fs.mkdir(folder, { recursive: true }, (err) => {
+    if (err) {
+      console.error("Failed to create folder", err)
+    }
+    callback()
+  })
+}
+
+function copyDir(sourceFolder, copyFolder) {
+  fs.readdir(sourceFolder, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error("Failed to read the folder", err)
+      return
+    }
+
+    files.forEach((file) => {
+      const source = path.join(sourceFolder, file.name)
+      const copy = path.join(copyFolder, file.name)
+
+      if (file.isDirectory()) {
+        copyDir(source, copy)
+      } else {
+        performCopy(source, copy)
+      }
+    })
+  })
+}
+
+function performCopy(source, copy) {
+  fs.copyFile(source, copy, (err) => {
+    if (err) {
+      console.error("Failed to copy file", err)
+      return
+    }
+
+    console.log(`Copied file "${source}" to "${copy}"`)
   })
 }
